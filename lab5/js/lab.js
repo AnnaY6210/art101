c0e3ccad72e5f6bab819259ab400d54d2a9dd96a
AnnaY6210 <77078866+AnnaY6210@users.noreply.github.com>
2021-01-26 17:04:00 -0800

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/*
+ * Author: Anna Yu
+ * Created: 26 January 2021
+ * License: Public Domain
+*/
+
+// Define Variables
+var make = "Honda";
+var model = "Civic";
+var color = "white";
+var year = 2014;
+
+// Calculate
+age = 2021 - 2014;
+
+// Change Color
+make = make.fontcolor("#c5c6c7");
+
+// Output
+document.writeln(("Make: " + make + "<br>").fontcolor("#c5c6c7"));
+document.writeln(("Model: " + model + "<br>").fontcolor("#c5c6c7"));
+document.writeln(("Color: " + color + "<br>").fontcolor("#c5c6c7"));
+document.writeln(("Year: " + year + "<br>").fontcolor("#c5c6c7"));
+document.writeln(("Age: " + age + " years<br>").fontcolor("#c5c6c7"));
