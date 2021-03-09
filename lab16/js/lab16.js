255a18331acd9b9ac80cf8c61fbae82086a8d999
AnnaY6210 <77078866+AnnaY6210@users.noreply.github.com>
2021-03-08 22:16:22 -0800

    bonus

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -4,6 +4,8 @@
  * License: Public Domain
 */
 
+var num = 1;
+
 function getajax (number) {
   $.ajax({
     url: `https://xkcd.com/${number}/info.0.json`,
@@ -20,17 +22,31 @@ function getajax (number) {
     var img = document.createElement('img');
     img.src = comicObj.img;
     img.id = "img";
+    $("#btn").hide();
     $("#output").show();
+
     $("#output").html("");
     $("#output").append("<h3>" + comicObj.title);
     $("#output").append(img);
     $("#output").append("<p>" + comicObj.alt);
     console.log(comicObj);
+
+    $("#output").append("<button class='button' id='minus'>Previous")
+    $("#output").append("<button class='button' id='plus'>Next")
   }
 
   function errorFun(xhr,status,strErr) {
     console.log("error "+strErr);
   }
 }
+$("#btn").click(getajax(num));
 
-$("#button").click(getajax(1));
+$("body").on("click", "#minus", function(){
+  num -= 1;
+  getajax(num);
+});
+
+$("body").on("click", "#plus", function(){
+  num += 1;
+  getajax(num);
+});
